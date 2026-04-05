5dab3868f558b3b2a8546cdba9193ea03f4b0f21
Lucas Lu <lucasxiao94@gmail.com>
2026-04-05 17:00:12 -0500

    Generated the new csv, made sure names matched with original data column names

--- fetch.py
+++ fetch.py
@@ -14,7 +14,7 @@ response = requests.get(URL, headers =
 
 data_json = response.json()
 data_df = pd.DataFrame(data_json['data'])
-print(data_df['pricing'][0])
+print(data_df['top_provider'][0])
 
 #prompt function to extract prompt pricing from the 'pricing' column
 def get_prompt(df):
@@ -28,9 +28,9 @@ def get_completion(df):
 def get_context_length(df):
     return df['top_provider']['context_length']
 def get_max_tokens(df):
-    return df['top_provider']['max_tokens']
+    return df['top_provider']['max_completion_tokens']
 def get_moderated(df):
-    return df['top_provider']['moderated']
+    return df['top_provider']['is_moderated']
 
 #createw new columns for prompt and completion pricing
 data_df['prompt_pricing'] = data_df.apply(get_prompt, axis=1)
@@ -38,8 +38,8 @@ data_df['completion_pricing'] = data_df.apply(get_completion, axis=1)
 
 #create new columns for context length, max tokens, and moderated
 data_df['context_length'] = data_df.apply(get_context_length, axis=1)
-data_df['max_tokens'] = data_df.apply(get_max_tokens, axis=1)
-data_df['moderated'] = data_df.apply(get_moderated, axis=1)
+data_df['max_completion_tokens'] = data_df.apply(get_max_tokens, axis=1)
+data_df['is_moderated'] = data_df.apply(get_moderated, axis=1)
 
 data_df = data_df.drop(columns=['pricing', 'top_provider'])
 

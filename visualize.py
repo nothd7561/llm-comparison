83880d2b7a8694216b605761e9f9315d3b709ae0
Lucas Lu <lucasxiao94@gmail.com>
2026-04-09 23:18:57 -0500

    adjusted radar graph visibility and tweaked yaxis scaling for bar charts

--- visualize.py
+++ visualize.py
@@ -11,10 +11,37 @@ def create_bar(data):
                                 x='Model Name', 
                                 y='Prompt Pricing (Tokens)',
                                 barmode='group')
-    
+    bar_chart.update_layout(
+        yaxis = dict(
+            range=[0, .00015]
+        )
+    )
     return bar_chart
 
-def create_grouped_bar(data):
+
+def create_bar_length(data):
+    #creates a bar chart using plotly express to compare the context length of the two models
+    bar_chart = px.bar(data, 
+                                x='Model Name', 
+                                y='Context Length',
+                                barmode='group')
+    bar_chart.update_layout(
+        yaxis = dict(
+            range=[0, 2000000]))
+    return bar_chart
+
+def create_bar_tokens(data):
+    #creates a bar chart using plotly express to compare the max completion tokens of the two models
+    bar_chart = px.bar(data, 
+                                x='Model Name', 
+                                y='Max Completion Tokens',
+                                barmode='group')
+    bar_chart.update_layout(
+        yaxis = dict(
+            range=[0, 1000192]))
+    return bar_chart
+
+#def create_grouped_bar(data):
     #creates a grouped bar chart using plotly express to compare the context length and max completion tokens of the two models
     melt_data = data.melt(id_vars=['Model Name'], value_vars=['Context Length', 'Max Completion Tokens'], var_name='Metrics', value_name='Amount(Tokens)')
     grouped_bar_chart = px.bar(melt_data, 
@@ -42,15 +69,25 @@ def create_radar(data):
     radar_chart.add_trace(go.Scatterpolar(
         r=[norm_length1, norm_tokens1, norm_pricing1, norm_completion1],
         theta=['Context Length', 'Max Completion Tokens', 'Prompt Pricing (Tokens)', 'Completion Pricing (Tokens)'],
-        name='Model 1',
+        name=data['Model Name'].values[0],
         fill='toself'
-    ))
+    ))#uwu
     radar_chart.add_trace(go.Scatterpolar(
         r=[norm_length2, norm_tokens2, norm_pricing2, norm_completion2],
         theta=['Context Length', 'Max Completion Tokens', 'Prompt Pricing (Tokens)', 'Completion Pricing (Tokens)'],
-        name='Model 2',
+        name=data['Model Name'].values[1],
         fill='toself'
     ))
+    
+    radar_chart.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                tickfont=dict(color='black')
+            )
+        )
+    )
+
+
     return radar_chart
 
 #calls the create_bar, create_grouped_bar, and create_radar functions on the compare_data variable and shows the resulting charts

e28517600b85431b49a90368e29c4e4c94a55945
Lucas Lu <lucasxiao94@gmail.com>
2026-04-17 19:32:20 -0500

    Added latency and output speed

--- backend/benchmark.py
+++ backend/benchmark.py
@@ -3,24 +3,31 @@ import requests
 import json
 import rapidfuzz
 
+#load the json file "cache.json" into a variable called data_json
 with open("cache.json") as f:
     data_json = json.load(f)
 
 
 def get_benchmark(model_name):
+    #create a list of all the AI model names
     name_list = []
     for model in data_json['data']:
         name_list.append(model['name'])
-
+    #use rapidfuzz to find the best match for the model_name in the name_list
     best_match = rapidfuzz.process.extractOne(model_name, name_list)[0]
+
+    #for each model in the data_json['data'] list, if the model's name matches the best_match,  
+    #return the values of the 'artificial_analysis_intelligence_index', 'artificial_analysis_coding_index', and 'artificial_analysis_math_index' keys from the 'evaluations' dictionary
     for model in data_json['data']:
         if model['name'] == best_match:
             intel_index = model['evaluations']['artificial_analysis_intelligence_index']
             code_index = model['evaluations']['artificial_analysis_coding_index']
             math_index = model['evaluations']['artificial_analysis_math_index']
-            return intel_index, code_index, math_index
-        
-print(get_benchmark("GPT 4"))
+            response_latency = model['median_time_to_first_token_seconds']
+            output_speed = model['median_output_tokens_per_second']
+            return intel_index, code_index, math_index, response_latency, output_speed
+
+print(get_benchmark("GPT 4.1"))
 
 
 
